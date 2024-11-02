a1c49b874e7b5b1b6ad40a71520c17a3032a9795
Charon De Beukelaer <hello@charond.be>
2024-11-02 14:02:58 +0100

    Added test module

---
+++ heimdall/heimdall.py
@@ -0,0 +1,13 @@
+from typing import Annotated
+from pydantic import Field
+from tracecat_registry import registry
+
+
+@registry.register(
+    default_title="Get Heimdall alerts",
+    display_group="Heimdall",
+    description="Get Heimdall alerts",
+    namespace="integrations.heimdall",
+)
+async def get_heimdall_alerts(value: str) -> dict[str, Any]:
+    return "Hello, world!"
