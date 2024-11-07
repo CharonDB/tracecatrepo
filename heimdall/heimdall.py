7040f562a95f798064495d33c6124ccf1526284e
Charon De Beukelaer <charon@telenet.be>
2024-11-07 23:30:22 +0100

    Black reformat

--- heimdall/heimdall.py
+++ heimdall/heimdall.py
@@ -5,8 +5,10 @@ import httpx
 
 
 heimdall_secret = RegistrySecret(
-    name = "heimdall",
-    keys = ["HEIMDALL_API_KEY", "HEIMDALL_CUSTOMER_ID"],)
+    name="heimdall",
+    keys=["HEIMDALL_API_KEY", "HEIMDALL_CUSTOMER_ID"],
+)
+
 
 @registry.register(
     default_title="Get DarkLayer alerts",
@@ -15,13 +17,14 @@ heimdall_secret = RegistrySecret(
     namespace="integrations.heimdall.darklayer",
     secrets=[heimdall_secret],
 )
-
 async def get_darklayer_alerts(
-    clientInfoId: Annotated[int | None, Field(description="The client ID (in case of querying a specific client)")],
+    clientInfoId: Annotated[
+        int | None,
+        Field(description="The client ID (in case of querying a specific client)"),
+    ],
     startDate: Annotated[str | None, Field(description="The start date for the query")],
-    endDate: Annotated[str | None, Field(description="The end date for the query")]
-    ) -> dict[str, Any]:
-
+    endDate: Annotated[str | None, Field(description="The end date for the query")],
+) -> dict[str, Any]:
 
     uri = "https://rc-dashboard.heimdalsecurity.com/api/heimdalapi/darklayerguard"
 
@@ -40,11 +43,11 @@ async def get_darklayer_alerts(
 
     async with httpx.AsyncClient() as client:
         response = await client.get(
-            uri, 
-            headers={"Authorization": "Bearer "+secrets.get("HEIMDALL_API_KEY")},
-            params=paramsjson
+            uri,
+            headers={"Authorization": "Bearer " + secrets.get("HEIMDALL_API_KEY")},
+            params=paramsjson,
         )
         response.raise_for_status()
         result = response.json()
 
-    return result             
+    return result
