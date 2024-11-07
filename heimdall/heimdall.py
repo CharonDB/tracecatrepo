10a1d4f86932764da1ee22426069110ca0c23ec7
Charon De Beukelaer <charon@telenet.be>
2024-11-07 22:09:57 +0100

    Refactor heimdall.py to remove unused code and fix syntax error in heimdall_secret definition

--- heimdall/heimdall.py
+++ heimdall/heimdall.py
@@ -1,5 +1,4 @@
 from typing import Annotated, Any
-from pydantic import Field
 from tracecat_registry import registry, RegistrySecret, secrets
 import httpx
 
@@ -8,16 +7,6 @@ heimdall_secret = RegistrySecret(
     name = "heimdall",
     keys = ["HEIMDALL_API_KEY", "HEIMDALL_CUSTOMER_ID"])
 
-
-@registry.register(
-    default_title="Get Heimdall alerts",
-    display_group="Heimdall",
-    description="Get Heimdall alerts",
-    namespace="integrations.heimdall",
-)
-async def get_heimdall_alerts(value: str) -> dict[str, Any]:
-    return "Hello, world!"
-
 @registry.register(
     default_title="Get DarkLayer alerts",
     display_group="Heimdall - DarkLayer",
@@ -25,6 +14,7 @@ async def get_heimdall_alerts(value: str) -> dict[str, Any]:
     namespace="integrations.heimdall.darklayer",
     secrets=[heimdall_secret],
 )
+
 async def get_darklayer_alerts(value: str) -> dict[str, Any]:
     clientInfoId: Annotated[int | None, Field(description="The client ID (in case of querying a specific client)")]
 
@@ -34,7 +24,7 @@ async def get_darklayer_alerts(value: str) -> dict[str, Any]:
         response = await client.get(
             uri, 
             headers={"Authorization": f"Bearer {secret}"},
-            params={"customerid": secret["HEIMDALL_CUSTOMER_ID"]}
+#            params={"customerid": secret["HEIMDALL_CUSTOMER_ID"]}
         )
         response.raise_for_status()
         return response.json()
