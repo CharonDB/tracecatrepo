212e9a54e4f170cc4fba4affbba8e12a6005e114
Charon De Beukelaer <charon@telenet.be>
2024-11-07 20:55:41 +0100

    Refactor get_darklayer_alerts.yml template for Heimdall integration

--- heimdall/heimdall.py
+++ heimdall/heimdall.py
@@ -1,6 +1,12 @@
 from typing import Annotated, Any
 from pydantic import Field
-from tracecat_registry import registry
+from tracecat_registry import registry, RegistrySecret, secrets
+import httpx
+
+
+heimdall_secret = RegistrySecret(
+    name = "heimdall",
+    keys = ["HEIMDALL_API_KEY", "HEIMDALL_CUSTOMER_ID"],)
 
 
 @registry.register(
@@ -11,3 +17,26 @@ from tracecat_registry import registry
 )
 async def get_heimdall_alerts(value: str) -> dict[str, Any]:
     return "Hello, world!"
+
+@registry.register(
+    default_title="Get DarkLayer alerts",
+    display_group="Heimdall - DarkLayer",
+    description="Get Heimdall alerts",
+    namespace="integrations.heimdall.darklayer",
+    secrets=[heimdall_secret],
+)
+async def get_darklayer_alerts(value: str) -> dict[str, Any]:
+    clientInfoId: Annotated[int | None, Field(description="The client ID (in case of querying a specific client)")]
+
+    secret = await secrets.get("heimdall")
+    uri = "https://rc-dashboard.heimdalsecurity.com/api/heimdalapi/darklayerguard"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            uri, 
+            headers={"Authorization": f"Bearer {secret}"},
+            params={"customerid": secret["HEIMDALL_CUSTOMER_ID"]}
+        )
+        response.raise_for_status()
+        return response.json()
+    
+                                                       
